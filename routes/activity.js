36aef7e8f6a2a01a599e64bcc0db35589cb0d0e5
surajmendhe5573 <surajmendhe470@gmail.com>
2025-05-10 17:03:55 +0530

    done

--- routes/activity.js
+++ routes/activity.js
@@ -2,7 +2,7 @@ const express= require('express');
 const { createActivity, listActivities } = require('../controllers/activity.controller');
 const router= express.Router();
 
-router.post('/create', createActivity);
+router.post('/book', createActivity);
 router.get('/', listActivities);
 
 
